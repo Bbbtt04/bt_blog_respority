92940ee422d1ef93b2638b9d4c25b0d20031f5de
Bbbtt04 <2393714045@qq.com>
2022-08-30 18:59:08 +0800

    update

---
+++ Vue3_demo/01_Proxy.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * const origin = {a: '100'};
+ * const obj = new Proxy(origin, {
+ *     get(target, p, receiver) {
+ *         if(p in target) {
+ *             return target[p]
+ *         } else {
+ *             throw new ReferenceError("error")
+ *         }
+ *     },
+ * })
+ *
+ * console.log(obj.a) //100
+ * console.log(obj.b) //error
+ * **/
+
+/*
+const target = {
+    _id: '1024',
+    name: 'vuejs'
+}
+
+const validators = {
+    name(val) {
+        return typeof val === 'string'
+    },
+    _id(val) {
+        return typeof val === 'number' && val > 1024
+    }
+}
+const createValidator = (target, validator) => {
+    return new Proxy(target, {
+        _valiator: validator,
+        set(target, p, value, receiver) {
+            let valiator = this._valiator[p](value)
+            if (valiator) {
+                return Reflect.set(target, p, value, proxy)
+            } else {
+                throw Error(`Cannot set ${p} to ${value}. Invalid type.`)
+            }
+        }
+    })
+}
+let proxy = createValidator(target, validators);
+proxy.name = 1
+ */
+update

c0a84b9b5212eaf45110bf3749e3f44ca8733def
Bbbtt04 <2393714045@qq.com>
2022-09-01 12:30:33 +0800

    update

---
+++ js手写题/07.bind.js
@@ -0,0 +1,14 @@
+Function.prototype.MyBind = function (context) {
+    if(typeof context !== 'function') {
+        throw new TypeError('Error')
+    }
+    const _this = this
+    const args = [...arguments].slice(1)
+    //返回一个函数
+    return function F(){
+        if(this instanceof F) {
+            return new _this(...args,...arguments)
+        }
+        return _this.apply(context,args.concat(...arguments))
+    }
+}

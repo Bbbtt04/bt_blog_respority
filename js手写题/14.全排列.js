0f6d7d63a4dd523e5beabbdbbcf722ee44742535
Bbbtt04 <2393714045@qq.com>
2022-09-18 18:30:49 +0800

    update

---
+++ js手写题/14.全排列.js
@@ -0,0 +1,18 @@
+const _permute = string => {
+  // 补全代码
+  let dfs = path => {
+      if(path.length === string.length) {
+          res.push(path)
+          return;
+      }
+      for(const char of string) {
+          if(path.indexOf(char) < 0) {
+              dfs(`${path}${char}`)
+          }
+      }
+  }
+  const res = []
+  dfs('')
+  return res;
+}
+console.log(_permute('abc'));

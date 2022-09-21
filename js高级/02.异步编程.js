1f2e9bf1fdc9f4f21ce4e1f503566f92e7e6a340
Bbbtt04 <2393714045@qq.com>
2022-09-21 14:37:59 +0800

    update

---
+++ js高级/02.异步编程.js
@@ -0,0 +1,14 @@
+let a = 0
+let b = async () => {
+  a = a + (await 10)
+  console.log('2', a)
+}
+b()
+a++
+console.log('1', a)
+/*
+  面试题: 说一说async和await特点?有什么优缺点?原理?
+  1. 一个函数加上async会返回一个Promise await接收返回值
+  2. 解决了链式调用问题  但是await失败后面的代码不会执行
+  3. async就是给函数包裹了一层Promise.resolve()  await内部实现了generator
+*/
---
+++ vue3源码学习/02.响应式原理.js
@@ -0,0 +1,48 @@
+let activeEffect
+function effect(fn) {
+  activeEffect = fn
+  fn()
+}
+// 存放副作用函数的桶
+const bucket = new WeakMap()
+let data = {}
+const obj = new Proxy(data, {
+  get(target, key) {
+    console.log(`${target}的${key} get`)
+    // 没有activeEffect直接return
+    // 根据target获取桶中的depsMap
+    let depsMap = bucket.get(target)
+    // 如果不存在depsMap 新建一个Map并与target关联
+    if (!depsMap) {
+      bucket.set(target, (depsMap = new Map()))
+    }
+    // 根据key获取deps
+    let deps = depsMap.get(key)
+    // 如果不存在deps 同样新建一个set与key关联
+    if (!deps) {
+      depsMap.set(key, (deps = new Set()))
+    }
+    deps.add(activeEffect)
+    return target[key]
+  },
+  set(target, key, newVal) {
+    console.log(`${target}的${key} set ${newVal}`)
+    // 设置新值
+    target[key] = newVal
+    // 获取target的depsMap
+    const depsMap = bucket.get(target)
+    if (!depsMap) return
+    const effects = depsMap.get(key)
+    effects && effects.forEach((fn) => fn())
+  },
+})
+obj.text = '1'
+let name
+function changeName() {
+  name = obj.text
+}
+effect(changeName)
+setTimeout(() => {
+  obj.text = '2'
+  console.log(name)
+}, 1000)

ef8a93d34e2f72df3bb6298df7d028ab0f16ac4a
Bbbtt04 <2393714045@qq.com>
2022-09-23 21:05:43 +0800

    update

--- js高级/02.异步编程.js
+++ js高级/02.异步编程.js
@@ -1,11 +1,11 @@
-let a = 0
-let b = async () => {
-  a = a + (await 10)
-  console.log('2', a)
-}
-b()
-a++
-console.log('1', a)
+// let a = 0
+// let b = async () => {
+//   a = a + (await 10)
+//   console.log('2', a)
+// }
+// b()
+// a++
+// console.log('1', a)
 /*
   面试题: 说一说async和await特点?有什么优缺点?原理?
   1. 一个函数加上async会返回一个Promise await接收返回值
